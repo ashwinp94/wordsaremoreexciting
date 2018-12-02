baa57c5cbc3a9ab4845608bf2f5cc6c61beec84d
Ashwin Prakash <ashp94@gmail.com>
2018-12-01 21:02:12 -0600

    final version of words-are-exciting

--- overly-excited.js
+++ overly-excited.js
@@ -1,14 +1,27 @@
-let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
 
-function addExcitement (theWordArray) {
+function addExcitement(theWordArray) {
 
     let buildMeUp = ""
 
     for (let i = 0; i < theWordArray.length; i++) {
-        buildMeUp += theWordArray[i] + " ";  
-        console.log(buildMeUp);
+        // "sentence[i]" can also be "thewordarray[i]"
+        if (i % 12 === 3) {
+            buildMeUp += sentence[i] + "! ";
+            console.log(buildMeUp);
+        }
+        else if (i % 12 === 6) {
+            buildMeUp += sentence[i] + "! ";
+            console.log(buildMeUp);
+        }
+        else if (i % 12 === 9) {
+            buildMeUp += sentence[i] + "! ";
+            console.log(buildMeUp);
+        } else {
+            buildMeUp += sentence[i] + " ";
+            console.log(buildMeUp);
+        }
     }
 }
-
 addExcitement(sentence)
